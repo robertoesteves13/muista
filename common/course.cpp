b0e7dde6a26750961c5aec6ad1961e03e8f159e8
Roberto Esteves <contact@robertoesteves.dev>
2024-06-02 18:43:52 -0300

    feat: make course serializable to XML

--- common/course.cpp
+++ common/course.cpp
@@ -1,9 +1,74 @@
 #include "common/course.hpp"
 
-Course::Course(QObject* parent, int id, QString name, QString description) : QObject(parent) {
+#include <QDebug>
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+
+Course::Course(QObject *parent, int id, QString name, QString description)
+    : QObject(parent) {
     this->id = id;
     this->name = name;
     this->description = description;
 }
 
 Course::~Course() {}
+
+void CourseSerializer::Serialize(QIODevice *device, Course *course) {
+    QXmlStreamWriter w(device);
+    w.setAutoFormatting(true);
+    w.writeStartDocument();
+
+    w.writeStartElement("Course");
+    w.writeAttribute("id", QString::number(course->Id()));
+    w.writeTextElement("Name", course->Name());
+    w.writeTextElement("Description", course->Description());
+    w.writeEndElement();
+
+    w.writeEndDocument();
+}
+
+Course *CourseSerializer::Deserialize(QIODevice *device, QObject *parent) {
+    QXmlStreamReader r(device);
+    QMap<QString, QString> map;
+
+    while (!r.atEnd()) {
+        auto t = r.readNext();
+        switch (t) {
+        case QXmlStreamReader::NoToken:
+            break;
+        case QXmlStreamReader::Invalid:
+            break;
+        case QXmlStreamReader::StartDocument:
+            break;
+        case QXmlStreamReader::EndDocument:
+            break;
+        case QXmlStreamReader::StartElement:
+            if (r.name().toString() == "Course") {
+                map.insert("id", r.attributes().value("id").toString());
+            }
+
+            if (r.name().toString() == "Name") {
+                map.insert("name", r.readElementText());
+            }
+
+            if (r.name().toString() == "Description") {
+                map.insert("description", r.readElementText());
+            }
+        case QXmlStreamReader::EndElement:
+            break;
+        case QXmlStreamReader::Characters:
+            break;
+        case QXmlStreamReader::Comment:
+            break;
+        case QXmlStreamReader::DTD:
+            break;
+        case QXmlStreamReader::EntityReference:
+            break;
+        case QXmlStreamReader::ProcessingInstruction:
+            break;
+        }
+    }
+
+    return new Course(parent, map["id"].toInt(), map["name"],
+                      map["description"]);
+}
--- include/common/course.hpp
+++ include/common/course.hpp
@@ -1,14 +1,32 @@
 #pragma once
 
 #include <QObject>
+#include <QProperty>
+#include <QQmlEngine>
 
 class Course : public QObject {
-  Q_OBJECT
-public:
-  Course(QObject* parent = nullptr, int id = 0, QString name = "???", QString description = "???");
-  ~Course();
+    Q_OBJECT
 
-public:
-    int id;
+    Q_PROPERTY(qint32 id READ Id)
+    Q_PROPERTY(QStringView name READ Name)
+    Q_PROPERTY(QStringView description READ Description)
+    QML_ELEMENT
+  public:
+    Course(QObject *parent = nullptr, int id = 0, QString name = "???",
+           QString description = "???");
+    ~Course();
+
+    int Id() { return id; }
+    QStringView Name() { return name; }
+    QStringView Description() { return description; }
+
+  private:
+    qint32 id;
     QString name, description;
+};
+
+class CourseSerializer {
+  public:
+    static void Serialize(QIODevice *device, Course *course);
+    static Course *Deserialize(QIODevice *device, QObject *parent);
 };
